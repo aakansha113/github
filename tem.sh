410f64f696692d2099046ce032018310af39cd1a
aakansha <aakanshahujare@gmail.com>
2025-02-03 17:40:24 +0000

    add

---
+++ tem.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "write your name here"
+read name
+echo "hello how are you $name?"
+
+
+echo "how are you"
+
+name="akshu"
+city="baner"
+echo "hello welcome to the devops course $name i am from $city"
